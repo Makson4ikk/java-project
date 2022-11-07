39a4a8b7c253272d5a3a9ab810b708865ec35eb8
Makson4ikk <90350685+Makson4ikk@users.noreply.github.com>
2022-11-07 22:21:36 +0200

    Update replacetext.java

--- src/oop/pogrebnyi04/replacetext.java
+++ src/oop/pogrebnyi04/replacetext.java
@@ -1,1 +1,38 @@
+package l04;
 
+/**
+ * Ввести текст. У тексті знайти та вивести всі слова-паліндроми (однаково читається в обох напрямках - зліва направо та справа наліво. 
+ * Наприклад: "noon", "civic", "radar", "level", "rotor", "refer").
+ * @author Pogrebnyi Maksym KN-921V
+ */
+
+public class replaceText {
+		
+	 static void replace(String s1, String s2) {
+				String st1 = Palindrome(s1);
+				String st2 = Palindrome(s2);
+				if (isPalindrome(st1,st2)==true)
+				{
+				    System.out.println("palindrome");
+				}
+				else {
+				    System.out.println("Not palindrome");
+				}
+			}
+
+		private static String Palindrome(String s) {
+		    StringBuilder stringBuilder = new StringBuilder(s);
+		    return String.valueOf(stringBuilder.reverse());
+		}
+
+		static boolean isPalindrome(String s1, String s2){
+		    if (s1.equals(s2))
+		    {
+		        return true;
+		    }
+		    else {
+		        return false;
+		    }
+		}
+
+	}
