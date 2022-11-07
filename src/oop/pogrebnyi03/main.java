98ba7a5c3b639101aa47f7d8b34b7cd1ce02fc20
Makson4ikk <90350685+Makson4ikk@users.noreply.github.com>
2022-11-07 19:23:39 +0200

    Update main.java

--- src/oop/pogrebnyi03/main.java
+++ src/oop/pogrebnyi03/main.java
@@ -1,95 +1,44 @@
 package l03;
 
 /**
- * Ввести декілька рядків. Упорядкувати, а потім вивести рядки за алфавітом (перший пріоритет) та в порядку зростання їх довжини (другий пріоритет).
+ * Ввести текст. У тексті знайти та вивести всі слова-паліндроми (однаково читається в обох напрямках - зліва направо та справа наліво. 
+ * Наприклад: "noon", "civic", "radar", "level", "rotor", "refer").
  * @author Pogrebnyi Maksym KN-921V
  */
 
-import java.util.*; 
-
-
+import java.util.*;
 public class l03 {
+	
 
-	private static Scanner scanner;
-	private static String textTwo;
-	private static String textOne;
-	
-	/**
-     Start function
-     */
-	
-	static void sortText (String textOne, String textTwo) {
-		System.out.print("\n");
-		sortAplphabet(textOne, textTwo);
-		count(textOne, textTwo);
-	}
-	
-	/**
-      Sorting text
-     */
-	
-	static void sortAplphabet (String textOne, String textTwo) {		
-		String letterOne = Character.toString(textOne.charAt(0));
-		String letterTwo = Character.toString(textTwo.charAt(0));
+	public static void main(String[] args) {
+		    Scanner s = new Scanner(System.in);
 
-		ArrayList<String> abc = new ArrayList<String>(); 
-		abc.add(letterOne); 
-		abc.add(letterTwo); 
+		    System.out.println("Enter the string");
 
-		Collections.sort(abc); 
-		Iterator<String> itr=abc.iterator();  
-		
-		if (itr.next() == letterOne) {
-			System.out.println("First letter is: "+letterOne);
-			System.out.println(textOne);
-			System.out.println("Second letter is: "+letterTwo);
-			System.out.println(textTwo);
-		} else {
-			System.out.println("Second letter is: "+letterTwo);
-			System.out.println(textTwo);
-			System.out.println("First letter is: "+letterOne);
-			System.out.println(textOne);
+		    String st1 = s.nextLine();
+
+		    String st2 = Palindrome(st1);
+		    if (isPalindrome(st1,st2)==true)
+		    {
+		        System.out.println("palindrome");
+		    }
+		    else {
+		        System.out.println("Not palindrome");
+		    }
 		}
-		System.out.println("\n");
 
-	}
-	/**
-     Count letter in sting
-     */
-	
-	static void count (String textOne, String textTwo) {
-		
-		int lengthOne = textOne.length();
-		int lengthTwo = textTwo.length();
-	
-		if (lengthOne > lengthTwo) {
-			System.out.println("Count length text One: " +  textOne.length());
-			System.out.println(textOne);
-			System.out.println("Count length text Two: " +  textTwo.length());
-			System.out.println(textTwo);
-
-		} else {
-			System.out.println("Count length text Two: " +  textTwo.length());
-			System.out.println(textTwo);
-			System.out.println("Count length text One: " +  textOne.length());
-			System.out.println(textOne);
+		private static String Palindrome(String s) {
+		    StringBuilder stringBuilder = new StringBuilder(s);
+		    return String.valueOf(stringBuilder.reverse());
 		}
-			
+
+		private static boolean isPalindrome(String s1, String s2){
+		    if (s1.equals(s2))
+		    {
+		        return true;
+		    }
+		    else {
+		        return false;
+		    }
+		}
 	}
- 	
-    /**
-     Console application entry point
-     */
-	
-    public static void main(String[] args) {
-
-    	scanner = new Scanner(System.in);
-		System.out.println("1 line: ");
-		textOne = scanner.nextLine();
-		System.out.println("2 line: ");
-		textTwo = scanner.nextLine();
-		
-		sortText(textOne, textTwo);
-    }
-
-}
