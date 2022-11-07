e7d71e2f465429394bc5c6441c1a5677ca19ed45
Makson4ikk <90350685+Makson4ikk@users.noreply.github.com>
2022-11-07 22:20:54 +0200

    Update main.java

--- src/oop/pogrebnyi04/main.java
+++ src/oop/pogrebnyi04/main.java
@@ -1,1 +1,17 @@
+package l04;
 
+/**
+	@author Pogrebnyi Maksym
+ */
+
+
+public class l04 {
+	
+
+	public static void main(String[] args) {
+		uiInterface uiInterface = new uiInterface();
+		uiInterface.start();
+	}
+
+
+}
