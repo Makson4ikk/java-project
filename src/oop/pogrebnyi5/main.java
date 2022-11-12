62ce12a2f2bbb98fbdcc1ba1f38123c6cac9e42b
Makson4ikk <90350685+Makson4ikk@users.noreply.github.com>
2022-11-12 20:12:20 +0200

    Update main.java

--- src/oop/pogrebnyi5/main.java
+++ src/oop/pogrebnyi5/main.java
@@ -1,1 +1,55 @@
+package l05;
 
+import java.util.*;
+
+public class l05 {
+
+	public static void main(String[] args) {
+		String str = "Буває таке мовчання коли слова не потрібні і це означає не кінець а початок розуміння"; 
+		
+	    String[] words = str.split(" ");
+	    
+	    ArrayList<String> arrayList1 = new ArrayList<> (); 
+	     for(String subStr:words) {
+	    	 arrayList1.add(subStr);
+	     }  
+
+	   
+	    System.out.println("toString():" + arrayList1.toString());
+	    
+	    System.out.println("add(String string):" + arrayList1.add("Джонатан Коу"));
+	    
+	    System.out.println("remove(String string):" + arrayList1.remove("початок розуміння"));
+	    
+	    String arraylist2[] = new String[arrayList1.size()];
+	    arraylist2 = arrayList1.toArray(arraylist2);
+	    arrayList1.toArray(arraylist2);
+	    for (String item : arraylist2) {
+	        System.out.println("Item in Object[] toArray() = " + item);
+	    }
+	    
+	    System.out.println("size():" + arrayList1.size());
+	    
+	    System.out.println("contains(String string):" + arrayList1.contains("Буває"));
+	    
+	    List<String> listTemp = new ArrayList<String>();
+        listTemp.add("Найпростіші речі");
+        listTemp.add("можуть змусити");
+        listTemp.add("вмирати від щастя.");
+	    System.out.println("containsAll(Container container):" + arrayList1.contains(listTemp));
+	    
+	    String wordDel = "слова";
+	    String word;
+	    
+	    Iterator<String> iterator = arrayList1.iterator();
+	   
+	    while(iterator.hasNext()) {
+	    	word = iterator.next();
+	    	System.out.println("Iterator has next, word is - " + word);
+	    	if (wordDel.equals(word)) {
+	    		iterator.remove();
+	    		System.out.println("Word '" + word + "' was be deleted");
+	    	}
+	    }
+	}
+}
