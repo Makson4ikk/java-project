5c6ee42af2a0115d1e71a85b766258935298ec42
Makson4ikk <90350685+Makson4ikk@users.noreply.github.com>
2022-11-20 20:07:29 +0200

    Create fullVacanises.java

---
+++ src/oop/pogrebnyi08/fullVacanises.java
@@ -0,0 +1,14 @@
+package l08;
+
+import java.util.List;
+
+public class fullVacanises {
+	private List<vacancies> vacancies;
+	
+	public List<vacancies> getVacancies () {
+		return vacancies;
+	}
+	public void setVacancies(List<vacancies> vacancies){
+		this.vacancies = vacancies;
+	}
+}
